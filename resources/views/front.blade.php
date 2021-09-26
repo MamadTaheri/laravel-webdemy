471b8fbee5baaacccd232c6cab61eaa8ef5bb19a
Mohammad Taheri <mamad.taheri.68@gmail.com>
2021-09-26 11:52:38 +0330

    make front.blade dynamic

--- resources/views/front.blade.php
+++ resources/views/front.blade.php
@@ -7,42 +7,25 @@
             <div>
                 <b-jumbotron header="BootstrapVue" lead="Bootstrap v4 Components for Vue.js 2">
                     <p>For more information visit website</p>
-                    <b-button variant="primary" href="#">Browse Course</b-button>
+                    <b-button variant="primary" href="{{route('series.index')}}">Browse Course</b-button>
                 </b-jumbotron>
             </div>
         </section>
-        
+
         <section>
             <div>
                 <b-card-group deck>
-                  <b-card title="Title" img-src="https://picsum.photos/300/300/?image=41" img-alt="Image" img-top>
-                    <b-card-text>
-                      This is a wider card with supporting text below as a natural lead-in to additional content.
-                      This content is a little bit longer.
-                    </b-card-text>
-                    <template #footer>
-                      <small class="text-muted">Last updated 3 mins ago</small>
-                    </template>
-                  </b-card>
+                    @foreach ($featuredSeries as $series)
+                         <b-card title={{ $series->title }} img-src="https://picsum.photos/300/300/?image=41" img-alt="Image" img-top>
+                            <b-card-text> {{ \Str::words($series->description, 10, '...')  }} </b-card-text>
+                            <template #footer>
+                                 <small class="text-muted">Last updated 3 mins ago</small>
+                            </template>
+                         </b-card>
+                    @endforeach
 
-                  <b-card title="Title" img-src="https://picsum.photos/300/300/?image=41" img-alt="Image" img-top>
-                    <b-card-text>
-                      This card has supporting text below as a natural lead-in to additional content.
-                    </b-card-text>
-                    <template #footer>
-                      <small class="text-muted">Last updated 3 mins ago</small>
-                    </template>
-                  </b-card>
 
-                  <b-card title="Title" img-src="https://picsum.photos/300/300/?image=41" img-alt="Image" img-top>
-                    <b-card-text>
-                      This is a wider card with supporting text below as a natural lead-in to additional content.
-                      This card has even longer content than the first to show that equal height action.
-                    </b-card-text>
-                    <template #footer>
-                      <small class="text-muted">Last updated 3 mins ago</small>
-                    </template>
-                  </b-card>
+
                 </b-card-group>
               </div>
         </section>
