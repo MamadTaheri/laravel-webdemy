84d90e8ef5a56129ca9fa5d846b715875755f684
Mohammad Taheri <mamad.taheri.68@gmail.com>
2021-09-26 12:02:02 +0330

    make components text-center

--- resources/views/front/series/index.blade.php
+++ resources/views/front/series/index.blade.php
@@ -8,11 +8,11 @@
             <div>
                 <b-row>
                     @foreach ($series as $s)
-                    <b-col cols="4">
+                    <b-col cols="4" class="text-center">
                         <b-card title={{ $s->title }} img-src="https://picsum.photos/300/300/?image=41" img-alt="Image" img-top>
                             <b-card-text> {{ \Str::words($s->description, 10)  }} </b-card-text>
-                            <template #footer>
-                                {{-- <b-button href="{{ route('series.show', $s) }}" variant="primary">Play</b-button> --}}
+                            <template justify="center" #footer>
+                                <b-button href="#" variant="primary">Play</b-button>
                            </template>
                         </b-card>
                     </b-col>
@@ -23,6 +23,7 @@
         </section>
 
         <section>
+            <h3 class="mb-3 text-center">Choose Plan that fits your need</h3>
             <pricing></pricing>
         </section>
 
