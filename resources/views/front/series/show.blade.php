7b1789fed703cea0a71fae8279f081fdca52c1ba
Mohammad Taheri <mamad.taheri.68@gmail.com>
2021-09-27 17:03:43 +0330

    add breadCrumbs to show blade file

--- resources/views/front/series/show.blade.php
+++ resources/views/front/series/show.blade.php
@@ -1,8 +1,9 @@
 @extends('layouts.app')
 
 @section('content')
-
 <b-container>
+    <b-breadcrumb :items="{{json_encode($breadCrumbs)}}">salam</b-breadcrumb>
+
     <section>
         <div class="jumbotron">
             <h1 class="display-4">{{$series->title}}</h1>
@@ -11,7 +12,7 @@
             <a class="btn btn-primary" href="#">Start Series</a>
             <a class="btn btn-success" href="#">Subscribe</a>
         </div>
-    </section> 
+    </section>
 
     <section class="mb-3">
         <h3 class="mb-3 text-center">Episodes</h3>
